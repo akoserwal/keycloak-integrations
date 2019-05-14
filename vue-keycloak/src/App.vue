117a9bc9d39c43bde080f371260b058d464dbff9
Abhishek Koserwal <akoserwa@redhat.com>
2019-05-14 15:14:02 +0530

    add vue-integration with keyclaok demo

---
+++ vue-keycloak/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" style="margin-left: 50px;">
+     <img alt="heart" src="./assets/heart.png" style="height: 100px;margin: 30px;">
+    <img alt="keycloak logo" src="./assets/keycloak.png">
+    <HelloWorld msg="Welcome to Your Vue.js App secured with Keycloak"/>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-keycloak/src/main.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueLogger from 'vuejs-logger';
+import * as Keycloak from 'keycloak-js'
+
+Vue.config.productionTip = false
+
+const options = {
+  isEnabled: true,
+  logLevel : Vue.config.productionTip  ? 'error' : 'debug',
+  stringifyArguments : false,
+  showLogLevel : true,
+  showMethodName : true,
+  separator: '|',
+  showConsoleColors: true
+};
+Vue.use(VueLogger, options);
+
+
+//keycloak init options
+let initOptions = {
+  url: 'https://0.0.0.0:8445/auth', realm: 'keycloak-demo', clientId: 'vue-test-app', onLoad:'login-required'
+}
+
+let keycloak = Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) =>{
+    
+    if(!auth) {
+      window.location.reload();
+    } else {
+      Vue.$log.info("Authenticated");
+
+    }
+ 
+    new Vue({
+      render: h => h(App),
+    }).$mount('#app')
+  
+
+    localStorage.setItem("vue-token", keycloak.token);
+    localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
+
+    setTimeout(() =>{
+      keycloak.updateToken(70).success((refreshed)=>{
+        if (refreshed) {
+          Vue.$log.debug('Token refreshed'+ refreshed);
+        } else {
+          Vue.$log.warn('Token not refreshed, valid for '
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+        }
+      }).error(()=>{
+          Vue.$log.error('Failed to refresh token');
+      });
+
+
+    }, 60000)
+
+}).error(() =>{
+  Vue.$log.error("Authenticated Failed");
+});
+
+
